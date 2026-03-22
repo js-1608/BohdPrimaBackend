fbafa9044fb4d1274415c97a830699c6b49eb8f1
js-1608 <2200520140049@ietlucknow.ac.in>
2026-03-22 16:15:55 +0530

    database

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-	const mongoUri = process.env.MONGO_URI || "MONGO_URI=mongodb+srv://shivamPractice:Utr%401010@cluster0.jianegr.mongodb.net/bohdpeima?retryWrites=true&w=majority";
+	const mongoUri = process.env.MONGO_URI;
 
 	if (!mongoUri) {
 		throw new Error("Missing MONGO_URI in environment variables");
